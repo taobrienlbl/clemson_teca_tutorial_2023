6082ff67cf0beacfa53e1661e37face4b7f8f2b1
Travis O'Brien <obrienta@iu.edu>
2023-05-14 15:26:04 -0600

    Added solution for 03; verified solution scripts run through all steps

--- 02_teca_bard/solution/00_link_files.sh
+++ 02_teca_bard/solution/00_link_files.sh
@@ -5,12 +5,10 @@ LINK_DIR=`pwd -P`/linked_files
 mkdir -p ${LINK_DIR}
 
 # enter the link directory
-# for this exercise, only link a subset - since we have limited nodes available and limited time, only run on the last ~45 years of data.
+# for this exercise, only link a subset - since we have limited nodes available and limited time, only run on the last ~5 years of data.
 pushd $LINK_DIR
-ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/PRECT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.PRECT.19[789]* .
-ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/PRECT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.PRECT.20* .
-ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/IVT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.IVT.19[789]* .
-ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/IVT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.IVT.20* .
+ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/PRECT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.PRECT.2010* .
+ln -s /glade/scratch/tobrien/CESM2-LE/atm/proc/tseries/hour_6/IVT/b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.IVT.2010* .
 # exit the link directory
 popd
 
